2742f1ba363ac4fc12c0944e42382287d038af32
Shogo Ogami <sg.tao.so@gmail.com>
2025-10-14 08:39:33 +0900

    feat:update clip_multi_screenshots page for show second only

--- src/pages/12_clip_multi_screenshots.py
+++ src/pages/12_clip_multi_screenshots.py
@@ -9,37 +9,61 @@ from PIL import Image
 import streamlit as st
 
 
-APP_TITLE = "📸 Multi Screenshot Selector (60s Clip)"
+APP_TITLE = "Multi Screenshot Selector (60s Clip)"
 
 
 def initialize_session_state():
+    if "tmp_path" not in st.session_state:
+        st.session_state.tmp_path = ""
+    if "generated_screens" not in st.session_state:
+        st.session_state.generated_screens = []
     if "screenshot_list" not in st.session_state:
         st.session_state.screenshot_list = []
 
 
-def extract_screenshots(video_bytes, start_minute=0):
-    """指定した分(start_minute)から60秒分のフレームを抽出"""
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as tmp:
-        tmp.write(video_bytes)
-        tmp_path = tmp.name
+class MultiScreenshot:
+    def __init__(self, video_bytes):
+        """指定した分(start_minute)から60秒分のフレームを抽出"""
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as tmp:
+            tmp.write(video_bytes)
+            self.tmp_path = tmp.name
+            st.session_state.tmp_path = tmp.name
+            # st.rerun()
 
-    clip = VideoFileClip(tmp_path)
+    def get_meta_info(self):
+        clip = VideoFileClip(st.session_state.tmp_path)
+        meta = {
+            "duration": clip.duration,
+            "fps": clip.fps,
+            "size": (clip.w, clip.h),
+        }
+        clip.close()
+        return meta
 
-    screenshots = []
-    start_time = start_minute * 60
-    end_time = min(start_time + 60, clip.duration)
+    def extract_screenshots(self, start_minute=0):
+        # clip = VideoFileClip(self.tmp_path)
+        clip = VideoFileClip(st.session_state.tmp_path)
+        screenshots = []
+        start_time = start_minute * 60
+        end_time = min(start_time + 60, clip.duration)
 
-    for i in range(int(start_time), int(end_time)):
-        frame = clip.get_frame(i)
-        image = Image.fromarray(frame)
-        img_bytes = BytesIO()
-        image.save(img_bytes, format="PNG")
-        img_bytes.seek(0)
-        screenshots.append((i, img_bytes))
+        for i in range(int(start_time), int(end_time)):
+            frame = clip.get_frame(i)
+            image = Image.fromarray(frame)
+            img_bytes = BytesIO()
+            image.save(img_bytes, format="PNG")
+            img_bytes.seek(0)
+            screenshots.append((i, img_bytes))
 
-    clip.close()
-    os.remove(tmp_path)
-    return screenshots
+        clip.close()
+        # os.remove(tmp_path)
+        return screenshots
+
+
+def _on_change_file_ms():
+    if st.session_state.tmp_path != "":
+        os.remove(st.session_state.tmp_path)
+        st.session_state.tmp_path = ""
 
 
 def download_zip(selected_list):
@@ -54,27 +78,40 @@ def download_zip(selected_list):
 
 def main():
     st.set_page_config(page_title=APP_TITLE)
-    st.title(APP_TITLE)
+    st.page_link("main.py", label="Back to Home", icon="🏠")
+    st.subheader(f"📹 {APP_TITLE}")
     initialize_session_state()
 
     uploaded_file = st.file_uploader(
-        "🎞 Upload MP4 file", type=["mp4", "mpeg4"]
+        "🎞 Upload MP4 file",
+        type=["mp4", "mpeg4"],
+        key="file_uploader_mshot",
+        on_change=_on_change_file_ms,
     )
     if not uploaded_file:
         st.info("動画ファイルをアップロードしてください。")
         return
 
     video_bytes = uploaded_file.read()
+    multi_shot = MultiScreenshot(video_bytes)
 
     # ▼ スタート分指定
+    meta = multi_shot.get_meta_info()
+    print(meta)
     start_minute = st.number_input(
-        "開始分（0 = 冒頭から）", min_value=0, step=1, value=0
+        f"Scraped Minite（0 = start, max_value={int(meta['duration']/60)})",
+        min_value=0,
+        max_value=int(meta["duration"] / 60),
+        step=1,
+        value=0,
     )
 
     if st.button("🖼 Generate 60 Screenshots"):
         st.info("スクリーンショットを生成中です。少々お待ちください…")
-        screenshots = extract_screenshots(
-            video_bytes, start_minute=start_minute
+        st.session_state.generated_screens = []
+        screenshots = multi_shot.extract_screenshots(
+            # video_bytes, start_minute=start_minute
+            start_minute=start_minute
         )
 
         st.session_state.generated_screens = screenshots
@@ -82,7 +119,8 @@ def main():
             f"{len(screenshots)} 枚のスクリーンショットを生成しました。"
         )
 
-    if "generated_screens" not in st.session_state:
+    # if "generated_screens" not in st.session_state:
+    if len(st.session_state.generated_screens) == 0:
         return
 
     screenshots = st.session_state.generated_screens
@@ -94,7 +132,9 @@ def main():
     for i, (timestamp, img_bytes) in enumerate(screenshots):
         col = cols[i % 5]
         with col:
-            checked = st.checkbox(f"{timestamp}s", key=f"chk_{timestamp}")
+            checked = st.checkbox(
+                label=f"{int(timestamp % 60)}s", key=f"chk_{timestamp}"
+            )
             st.image(
                 img_bytes,
                 # use_container_width=True
